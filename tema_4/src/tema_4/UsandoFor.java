9193b8a007364e8eeff8333b4c1974be92c24b4a
User <User@DESKTOP-RHBM60S>
2023-01-16 13:06:42 -0500

    listo for

---
+++ tema_4/src/tema_4/UsandoFor.java
@@ -0,0 +1,13 @@
+package tema_4;
+
+public class UsandoFor {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		int numeroFor=0;
+		for(int i=numeroFor;i<=3;i++) {
+			System.out.println("ejecucion "+i);
+		}
+	}
+
+}
