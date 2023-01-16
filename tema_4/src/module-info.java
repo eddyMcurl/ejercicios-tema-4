bfe857837de3355cfd3c38c4ff89941230cbbbe3
User <User@DESKTOP-RHBM60S>
2023-01-16 12:37:36 -0500

    Usando if listo

---
+++ tema_4/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author User
+ *
+ */
+module tema_4 {
+}
---
+++ tema_4/src/tema_4/UsandoIf.java
@@ -0,0 +1,16 @@
+package tema_4;
+
+public class UsandoIf {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		int numeroIf =0;
+		if(numeroIf==0)
+			System.out.println("es cero");
+		if (numeroIf<0)
+			System.out.println("es negativo");
+		if(numeroIf>0)
+			System.out.println("es positivo");
+	}
+
+}
