44581289358e712ccb6256cefebd2f1731f5af4d
User <User@DESKTOP-RHBM60S>
2023-01-16 13:04:16 -0500

    listo do while

---
+++ tema_4/src/tema_4/UsandoDowhile.java
@@ -0,0 +1,14 @@
+package tema_4;
+
+public class UsandoDowhile {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		int numeroWhile=0;
+		do {
+			System.out.println("iteraccion "+numeroWhile);
+			numeroWhile++;
+		} while (numeroWhile<1);
+	}
+
+}
---
+++ tema_4/src/tema_4/Usandowhile.java
@@ -0,0 +1,14 @@
+package tema_4;
+
+public class Usandowhile {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		int numeroWhile=0;
+		while (numeroWhile<3) {
+			System.out.println("iteraccion "+numeroWhile);
+			numeroWhile++;
+		}
+	}
+
+}
